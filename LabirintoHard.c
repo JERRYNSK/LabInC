6b3202e30a96c822b92aeb0307d17b4bbbf2903d
JERRYNSK <jerryoliveira909@gmail.com>
2025-12-01 11:06:18 -0300

    receba

--- LabirintoHard.c
+++ LabirintoHard.c
@@ -3,96 +3,144 @@
 #include<string.h>
 
 void transformLabInBy(char lab[12][12], int **pathBy, int l, int c);
+int verifyDirection(int **path, int *coords, int lenLine, int lenCol);
+void walk(int **caminhante, int **mapa, int **caminho, int *indiceCaminho,int linha, int coluna);
+int recursiveImplement(int **caminhante, int **mapa, int **caminho, int lenLine, int lenCol, int *index);
+int isCaminhavel(int **mapa, int l, int c);
+
 int **allocM(int l, int c);
 void desalloc(int **mat, int len);
-void countSteps(int **path, int l, int c, int *steps);
-void searchPath(int **path, int **coords, int l, int c);
+
 int main(){
     char labirinto[12][12] = {
         "************",
-        "*     *    *",
-        "* *** * ** *",
+        "* S   *    *",
+        "* **** **  *",
         "* *     *  *",
-        "* * *** ** *",
+        "* ***** ** *",
         "*   *      *",
         "*** * **** *",
         "*   *    * *",
         "* *** ** * *",
         "*   *  *   *",
-        "* *** **** S",
+        "***** ****  ",
         "E          *"
     };
-    //sera passos dado por vectors2d
-    int steps =0;
 
-
-    //parede = 0, caminho livre = 1, entrada = -1, saida = 2
     int **caminhoBinario = allocM(12, 12);
     transformLabInBy(labirinto, caminhoBinario, 12,12);
-    countSteps(caminhoBinario, 12,12, &steps);
-    int **vector2d = allocM(steps, 2);
+    int **vector2d = allocM(1000, 2);
+    int **caminhante = allocM(1, 2);
 
-    //demarcar a entrada
+    int index = 1;
+
     for(int i =0; i < 12; i++){
         for(int j=0; j < 12; j++){
             if(caminhoBinario[i][j] == -1){
                 vector2d[0][0] = i;
                 vector2d[0][1] = j;
-
             }
         }
     }
+
     printf("entrada está em: (%d, %d)\n", vector2d[0][0], vector2d[0][1]);
 
-    searchPath(caminhoBinario, vector2d, 12,12);
-    for(int i=0; i < 12; i++){
-        for(int j=0; j < 12; j++){
-            printf("[%d]", caminhoBinario[i][j]);
+    caminhante[0][0] = vector2d[0][0];
+    caminhante[0][1] = vector2d[0][1];
+
+    recursiveImplement(caminhante, caminhoBinario, vector2d, 12, 12, &index);
+
+    printf("a saida está em (%d, %d)\n", vector2d[index - 1][0], vector2d[index - 1][1]);
+
+    for(int i = 0; i < 12; i++){
+        for(int j =0; j<12; j++){
+            if(caminhoBinario[i][j] == 3) printf("[#]");
+            else if(caminhoBinario[i][j] == 1) printf("[1]");
+            else printf("[0]");
+        }
+        printf("\n");
+    }
+
+    char tracePath[12][12];
+
+    for(int i=0; i< 12; i++){
+        for(int j =0; j < 12; j++){
+            tracePath[i][j] = '1';
+        }
+    }
+
+    for(int k =0; k<index; k++){
+        int line = vector2d[k][0];
+        int col  = vector2d[k][1];
+        tracePath[line][col] = '#';
+    }
+
+    for(int i=0; i< 12; i++){
+        for(int j =0; j < 12; j++){
+            printf("%c", tracePath[i][j]);
         }
         printf("\n");
     }
 }
-void searchPath(int **path, int **coords, int l, int c){
-    int x =0, y = 0, step = 0;
-    //origem
-    x = coords[0][0];
-    y = coords[0][1];
-    do {
-        //verificar 8 direçoes
-        //verificar direita
-        if(x + 1 < 12 && path[x + 1][y] == 1){
-            x++;
-            step++;
-            coords[step][0] = x;
-            coords[step][1] = y;
-        //verificar diagonal direita
-        } else if(x + 1 < 12 && y + 1 < 12 && path[x + 1][ y+1] == 1){
-            x++;
-            y++;
-            step++;
-            coords[step][0] = x;
-            coords[step][1] = y;
-        //verificar baixo(baixo é incrementando
-        } else if(y + 1 < 12 && path[x][y + 1] == 1){
 
-        }
-
-
-    } while(path[x][y] != 2);
-}
-void backOne(int **path, int **coords, int *x, int *y){
-
-}
-void countSteps(int **path, int l, int c, int *steps){
-    for(int i =0; i < l; i++){
-        for(int j = 0; j< c; j++){
-            if(path[i][j] == 1){
-                *steps += 1;
-            }
-        }
+int recursiveImplement(int **caminhante, int **mapa, int **caminho, int lenLine, int lenCol, int *index)
+{   
+    static int step = 1;
+    int linePos = caminhante[0][0], colPos = caminhante[0][1];
+    
+    if (linePos - 1 >= 0 && isCaminhavel(mapa, linePos - 1, colPos)){
+        linePos -= 1;
     }
-
+    else if(linePos - 1 >= 0 && colPos + 1 < lenCol && isCaminhavel(mapa, linePos - 1, colPos + 1)){
+        linePos -= 1;
+        colPos += 1;
+    }
+    else if(colPos + 1 < lenCol && isCaminhavel(mapa, linePos, colPos + 1)){
+        colPos += 1;
+    }
+    else if(linePos + 1 < lenLine && colPos + 1 < lenCol && isCaminhavel(mapa, linePos + 1, colPos + 1)){
+        linePos += 1;
+        colPos += 1;
+    }
+    else if (linePos + 1 < lenLine && isCaminhavel(mapa, linePos + 1, colPos)){
+        linePos += 1;
+    }
+    else if(linePos + 1 < lenLine && colPos - 1 >= 0 && isCaminhavel(mapa, linePos + 1, colPos - 1)){
+        linePos += 1;
+        colPos -= 1;
+    }
+    else if(colPos - 1 >= 0  && isCaminhavel(mapa, linePos, colPos - 1)){
+        colPos -= 1;
+    }
+    else if(linePos - 1 >= 0 && colPos - 1 >= 0 && isCaminhavel(mapa, linePos - 1, colPos - 1)){
+        linePos -= 1;
+        colPos -= 1;
+    } else {
+        if(step <= 1) return 0;
+        step--;
+        linePos = caminho[step][0];
+        colPos = caminho[step][1];
+    }
+    
+    caminhante[0][0] = linePos;
+    caminhante[0][1] = colPos; 
+    if (mapa[linePos][colPos] == 1) {
+        mapa[linePos][colPos] = 3;
+        caminho[step][0] = linePos;
+        caminho[step][1] = colPos;
+        step++;
+    }
+    *index = step;
+    
+    if (mapa[linePos][colPos] == 2) return 1;
+    return recursiveImplement(caminhante, mapa, caminho, lenLine, lenCol, index);
 }
+
+int isCaminhavel(int **mapa, int line, int col){
+    if(mapa[line][col] == 1 || mapa[line][col] == 2) return 1;
+    return 0; 
+}
+
 void transformLabInBy(char lab[12][12], int **pathBy, int l, int c){
     if (pathBy != NULL){
         for(int i =0; i < l; i++){
@@ -111,14 +159,11 @@ void transformLabInBy(char lab[12][12], int **pathBy, int l, int c){
                         pathBy[i][j] = 2;
                     break;
                     default:
-                    //considere que já foi visto
                         pathBy[i][j] = 404;
-
                 }
             }
         }
     }
-
 }
 
 int **allocM(int line, int col){
@@ -136,10 +181,10 @@ int **allocM(int line, int col){
     }
     return mat;
 }
+
 void desalloc(int **mat, int len){
     for(int i=0; i <= len; i++){
         free(mat[i]);
     }
     free(mat);
-
 }
